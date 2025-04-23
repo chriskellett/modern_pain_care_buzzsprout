24b32d8fa4917e86914990b821af5d65898179e2
Chris Kellett <53143184+chriskellett@users.noreply.github.com>
2025-04-23 16:28:50 +0100

    Update cors.ts

--- cors.ts
+++ cors.ts
@@ -2,7 +2,7 @@
 
 // Define CORS headers to allow cross-origin requests
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*", // Change to specific origins in production
+  "Access-Control-Allow-Origin": "https://exact-structure-510787.framer.app/", // Change to specific origins in production
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Access-Control-Max-Age": "86400", // 24 hours
